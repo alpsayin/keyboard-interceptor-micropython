0a567a444b0d2ca18ca9fac26cddc1bc9d7c09b1
Alp Sayin <alpsayin@gmail.com>
2020-12-21 00:11:47 +0300

    References to actually implement processing of keyscan codes in the future

--- keyscan.py
+++ keyscan.py
@@ -1,6 +1,8 @@
 
+# References to actually implement processing of keyscan codes in the future
 # https://techdocs.altium.com/display/FPGA/PS2+Keyboard+Scan+Codes
 # https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
+# https://www.nutsvolts.com/magazine/article/get-ascii-data-from-ps-2-keyboards
 
 def keyscan_to_utf8(captured_raw):
     ret_str = ''
