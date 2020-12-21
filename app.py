42eceb669c6233571eeae53ebcd386ead5775c41
Alp Sayin <alpsayin@gmail.com>
2020-12-21 14:01:58 +0300

    refactor to implement timer callbacks as tasks

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import machine, time, os
-import _thread
-from machine import Timer, WDT, Pin
-from micropython import const, mem_info
+import machine
+import time
+from machine import Timer
+from micropython import const
 import gc
 import repl_drop
 import wlan_wrapper
@@ -25,7 +25,6 @@ from credentials import WLAN_SSID, WLAN_KEY
 DHCP_HOSTNAME = 'espresso0'
 
 # MQTT
-mqtt_fail = False
 MQTT_HOSTNAME = 'alpcer0.local'
 MQTT_TOPIC = 'kybIntcpt'
 
@@ -33,6 +32,7 @@ MQTT_TOPIC = 'kybIntcpt'
 SCK_PIN = const(14)  # Outside jumper IO14
 
 # status
+heartbeat_timer_flag = True
 heartbeat = Timer(-1)
 status_dict = dict(
     hostname='null',
@@ -44,17 +44,30 @@ status_dict = dict(
 )
 
 # publish timer
+publish_timer_flag = False
 publish_period = 5  # seconds
 publish_timer = Timer(-2)
 
 # capture buffer
 capture_buffer = bytearray()
 
-# frequency counter
-frequency_counter = None
+
+def update_auto_baudrate(new_freq):
+    global status_dict
+    if not status_dict['autobaud']:
+        return
+    # if new_freq < 9e3:
+    #     return
+    # if new_freq > 17e3:
+    #     return
+    freq_diff = abs(uart_wrapper.baudrate - new_freq)
+    if freq_diff / new_freq >= 0.1:
+        print('Setting baudrate to {}kHz'.format(new_freq))
+        uart_wrapper.update_baudrate(new_freq)
+        status_dict['freq'] = uart_wrapper.baudrate
 
 
-def check_uart():
+def check_uart(new_freq):
     global status_dict, capture_buffer
     if(0 == uart_wrapper.raw_uart.any()):
         return
@@ -66,18 +79,24 @@ def check_uart():
         if status_dict['passthrough']:
             uart_wrapper.raw_uart.write(captured_raw)
         capture_buffer.extend(captured_raw)
+    update_auto_baudrate(new_freq)
 
 
 def flush_buffer():
-    global capture_buffer, mqtt_fail
+    global capture_buffer
+    mqtt_fail = False
     captured, processed_len = keyscan_to_mqtt(capture_buffer)
     capture_buffer = capture_buffer[processed_len:]
     if len(captured) != 0:
         try:
-            mqtt_wrapper.mqtt_client.publish(MQTT_TOPIC, captured)
+            mqtt_wrapper.mqtt_client.publish(
+                MQTT_TOPIC,
+                '# {}'.format(captured)
+            )
         except OSError as ose:
             print('MQTT publish error: {}'.format(ose))
             mqtt_fail = True
+    return mqtt_fail
 
 
 def simulate_capture(simulated_capture_str):
@@ -100,8 +119,8 @@ def disable_autobaud(splitted):
     forced_baud = -1
     try:
         forced_baud = int(splitted)
-    except:
-        print('Invalid baud message received: {}'.format(splitted))
+    except OSError as ose:
+        print('{} Invalid baud message received: {}'.format(ose, splitted))
     if forced_baud != -1:
         status_dict.update(autobaud=False, freq=forced_baud)
         uart_wrapper.update_baudrate(forced_baud)
@@ -168,30 +187,6 @@ def on_mqtt_msg_received(topic, msg):
     handle_cmd(msg)
 
 
-
-
-def update_auto_baudrate(new_freq):
-    global status_dict
-    if not status_dict['autobaud']:
-        return
-    # if new_freq < 9e3:
-    #     return
-    # if new_freq > 17e3:
-    #     return
-    freq_diff = abs(uart_wrapper.baudrate - new_freq)
-    if freq_diff / new_freq >= 0.1:
-        print('Setting baudrate to {}kHz'.format(new_freq))
-        uart_wrapper.update_baudrate(new_freq)
-        status_dict['freq'] = uart_wrapper.baudrate
-
-
-def heartbeat_callback(timer_obj):
-    global frequency_counter
-    frequency_counter.average_samples()
-    update_auto_baudrate(frequency_counter.freq_hz)
-    print_status()
-
-
 def prepare_status_string():
     global status_dict
     status_dict['mem_free'] = gc.mem_free()
@@ -200,28 +195,20 @@ def prepare_status_string():
     )
 
 
-def publish_timer_callback(timer_obj):
-    global status_dict, mqtt_fail
-    try:
-        mqtt_wrapper.mqtt_client.publish(
-            MQTT_TOPIC,
-            '# {} {}'.format(
-                status_dict['hostname'],
-                prepare_status_string()
-            )
-        )
-    except OSError as ose:
-        print('MQTT periodic publish error: {}'.format(ose))
-        mqtt_fail = True
-    gc.collect()
-
-
 def print_status():
     global status_dict
     print(prepare_status_string())
     status_dict['seconds'] += 1
 
 
+def heartbeat_callback(timer_obj):
+    global heartbeat_timer_flag
+    heartbeat_timer_flag = True
+
+
+def publish_timer_callback(timer_obj):
+    global publish_timer_flag
+    publish_timer_flag = True
 
 
 def init_mqtt():
@@ -254,12 +241,12 @@ def init_publish_timer():
 
 
 def init_frequency_counter():
-    global frequency_counter
     frequency_counter = FreqCounter(pin_number=SCK_PIN)
+    return frequency_counter
 
 
 def main_init():
-    global display, wlan
+    global status_dict
     machine.freq(DEVICE_FREQ)
 
     if(wlan_wrapper.init_wifi(WLAN_SSID, WLAN_KEY, DHCP_HOSTNAME, timeout=None)):
@@ -278,28 +265,70 @@ def main_init():
     init_publish_timer()
     init_heartbeat_timer()
 
-    init_frequency_counter()
+    frequency_counter = init_frequency_counter()
 
-    return 0
+    return frequency_counter
+
+
+def heartbeat_task(frequency_counter):
+    frequency_counter.average_samples()
+    print_status()
+
+
+def publish_task():
+    mqtt_fail = False
+    try:
+        mqtt_wrapper.mqtt_client.publish(
+            MQTT_TOPIC,
+            '# {} {}'.format(
+                status_dict['hostname'],
+                prepare_status_string()
+            )
+        )
+    except OSError as ose:
+        print('MQTT periodic publish error: {}'.format(ose))
+        mqtt_fail = True
+    gc.collect()
+    return mqtt_fail
+
+
+def mqtt_task():
+    try:
+        mqtt_wrapper.mqtt_client.check_msg()
+        mqtt_fail = False
+    except OSError as ose:
+        print('MQTT check message failed: {}'.format(ose))
+        mqtt_fail = True
+    return mqtt_fail
+
+
+def mqtt_fail_handler_task(mqtt_fail):
+    print('Trying to re-establish connection.')
+    wlan_wrapper.restart_wifi(WLAN_SSID, WLAN_KEY, DHCP_HOSTNAME, 3)
+    init_mqtt()
 
 
 def main():
-    global mqtt_fail
+    global heartbeat_timer_flag, publish_timer_flag, status_dict
     repl_drop.wait(BOOT_TIME)
     print('app.py')
-    main_init()
+    mqtt_fail = False
+    frequency_counter = main_init()
     while True:
         # MQTT Task
-        try:
-            mqtt_wrapper.mqtt_client.check_msg()
-        except OSError as ose:
-            print('MQTT check message failed: {}'.format(ose))
-            mqtt_fail = True
+        mqtt_fail |= mqtt_task()
         # UART Task
-        check_uart()
+        check_uart(frequency_counter.freq_hz)
+        # Periodic Heartbeat Task
+        if heartbeat_timer_flag:
+            heartbeat_timer_flag = False
+            heartbeat_task(frequency_counter)
+        # Periodic Publish Task
+        if publish_timer_flag:
+            publish_timer_flag = False
+            mqtt_fail |= publish_task()
         # MQTT fail handler
         if mqtt_fail:
-            print('Trying to re-establish connection.')
             mqtt_fail = False
-            init_mqtt()
+            mqtt_fail_handler_task()
         time.sleep(0.005)
